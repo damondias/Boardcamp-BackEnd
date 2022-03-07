4eb6a88502ddbde1a7c8bb7dfd35608fe869483a
Damon <damondias@gmail.com>
2022-03-07 00:49:19 -0400

    add POST and GET routes from clients

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import categoriesRouter from "./categoriesRouter.js";
 import gamesRouter from "./gamesRouter.js";
+import customersRouter from "./customersRouter.js";
 
 const router = express.Router();
 
 router.use(categoriesRouter);
 router.use(gamesRouter);
+router.use(customersRouter);
 
 export default router;
